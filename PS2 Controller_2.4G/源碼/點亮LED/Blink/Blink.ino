7cb99a091ddbdd46a422a3dd50c5fb342eee1db8
Anson Au <ash1712@gmail.com>
2020-11-30 00:11:00 +0800

    init

---
+++ PS2 Controller_2.4G/源碼/點亮LED/Blink/Blink.ino
@@ -0,0 +1,9 @@
+void setup() {
+  pinMode(13, OUTPUT);//设定数字13管脚为输出
+}
+void loop() {
+  digitalWrite(13, HIGH); //数字13管脚输出高电平
+  delay(1000); //延时1000ms                      
+  digitalWrite(13, LOW);//数字13管脚输出低电平
+  delay(1000); //延时1000ms
+}
